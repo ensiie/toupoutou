a67cc079d14bb6718788c54e83b2d97b0ddbf99a
Paul Chobert <paul@chobert.fr>
2011-12-01 00:25:14 +0100

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toupoutou::Application.config.secret_token = '809e01923a5a10cad8a478ebbd3ce72aac10118f2d301c009342787351074aff5ccd8a217d5aef7fa165be89144a63d0524c4b534874519a3f5e73da606670b2'
