bcad6ace33c31e7789d9e402e4c2f76412e75c76
Paul Chobert <paul@chobert.fr>
2011-12-01 03:34:29 +0100

    add unicorn staging config

---
+++ config/unicorn_staging.rb
@@ -0,0 +1,14 @@
+APP_PATH = "/home/toupoutou/www/staging/"
+
+worker_processes 2
+
+working_directory APP_PATH + "current/"
+
+listen "/tmp/toupoutou.socket", :backlog => 64
+
+timeout 30
+
+pid APP_PATH + "shared/pids/unicorn.pid"
+
+stderr_path APP_PATH + "shared/log/unicorn.stderr.log"
+stdout_path APP_PATH + "shared/log/unicorn.stdout.log"
