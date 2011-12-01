7bdad09d399de4b79b712a75188bc19a7d5cb908
Paul Chobert <paul@chobert.fr>
2011-12-01 23:59:29 +0100

    add resque confid

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,8 +11,8 @@ require 'capistrano/ext/multistage'
 require 'bundler/capistrano'
 
 after "deploy:setup", "symlinks:mkdir"
-after "deploy:update_code", "symlinks:db"
-after "deploy", "deploy:cleanup"
+after "deploy:update_code", "symlinks:db", "symlinks:omniauth_config"
+after "deploy", "deploy:restart_workers", "deploy:cleanup"
 
 namespace :symlinks do
   desc "Symlink database config file"
@@ -20,9 +20,22 @@ namespace :symlinks do
     run "ln -nfs #{shared_path}/config/mongoid.yml #{current_release}/config/mongoid.yml"
   end
 
+  task :omniauth_config do
+    run "ln -nfs #{shared_path}/config/omniauth_config.yml #{current_release}/config/omniauth_config.yml"
+  end
+
   desc "Create dirs"
   task :mkdir do
     run "mkdir -p #{shared_path}/config"
+  end
+end
+
+namespace :restart_workers do
+  desc "Restart Resque Workers"
+  task :restart_workers, :roles => :app do
+    rake = fetch(:rake, "rake")
+    rails_env = fetch(:rails_env, "production")
+    run "cd #{current_path} && SHARED_PATH=#{shared_path} RAILS_ENV=#{rails_env} RAILS_ROOT=#{current_path} bundle exec god -c resque.god restart"
   end
 end
 
