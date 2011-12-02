952d897dcc65f7723f8b95405726e62f89fd200b
Léo Unbekandt <leo@unbekandt.eu>
2011-12-02 01:29:29 +0100

    Suppression utilisateurs OK

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,11 +1,11 @@
 class FriendsController < ApplicationController
 	def index
-		@friends = Array.new
-		user = current_user
-		user.friends.each do |f|
-			if user.id == f.id then
-				@friends.push user
-			end
-		end
+		@friends = current_user.friends
+	end
+
+	def destroy
+		@friend = User.find(params[:id])
+		@friend.destroy
+		redirect_to friends_path
 	end
 end
