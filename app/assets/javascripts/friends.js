568f37f7a71e360d0bda8baea533a45a5f16aef8
Adrian <braciglianoadrian@gmail.com>
2011-12-02 05:46:09 +0100

    Raccourcit

---
+++ app/assets/javascripts/friends.js
@@ -0,0 +1,10 @@
+$(document).ready(function () { 
+
+    $(".input_invit").focus();
+    
+    // Accès tableau de bord
+    $(".input_invit").bind('keydown', 'return', function () {
+        alert(' Ajout reussi ');
+    });
+    
+})
