16fc8e1089b0b17fb87ba7492c08f96007d9ec9c
Léo Unbekandt <leo@unbekandt.eu>
2011-12-02 07:55:52 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,8 @@
 User.create email: 'dev@toupoutou.fr', :password => 'toupoutou'
+
+a = User.create :email => "tou@poutou.pou", :password => "password", :first_name => "Tou", :last_name => "Poutou", :birthday => Date.parse("12/12/2000")
+a.friends.create :email => "test@test.com", :password => "password", :first_name => "Leo", :last_name => "Unbekandt", :birthday => Date.parse("9/12/1992")
+a.friends.create :email => "test2@test.com", :password => "password", :first_name => "Lucas", :last_name => "Bee", :birthday => Date.parse("4/12/1004")
+a.friends.create :email => "test3@test.com", :password => "password", :first_name => "Victor", :last_name => "Bahl", :birthday => Date.parse("8/12/1004")
+a.friends.create :email => "test4@test.com", :password => "password", :first_name => "Paul", :last_name => "Chobert", :birthday => Date.parse("15/12/1004")
+
