2d4e87eb4a7eaae8179a59f5c8d9a9ffb1ac512c
Sebastien Azimi <sebstp@gmail.com>
2011-12-02 01:51:46 +0100

    Facebook friends import

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,11 @@
 class FriendsController < ApplicationController
   def import
+    @facebook_friends = current_user.facebook_friends
+  end
+
+  def validate_import
+    @friends = params[current_user][:friends]
+    current_user.friends += @friends
   end
 
 	def index
